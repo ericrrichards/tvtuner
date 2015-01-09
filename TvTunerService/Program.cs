867f49a2b51e6ff08ba5f5e9eb1bfaeb5ed96e2e
ericrrichards@gmail.com <ericrrichards@gmail.com>
2015-01-08 22:13:29 -0500

    start on self-hosted version of tvtuner

---
+++ TvTunerService/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TvTunerService {
+    static class Program {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        private static void Main(string[] args) {
+            if (args.Length > 0) {
+                var svc = new Service1();
+                svc.Start();
+
+                Console.WriteLine("Press enter to terminate...");
+                Console.ReadLine();
+                svc.End();
+
+            }
+            else {
+                var servicesToRun = new ServiceBase[] {
+                    new Service1()
+                };
+                ServiceBase.Run(servicesToRun);
+            }
+        }
+    }
+}
